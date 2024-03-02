3b5cb600eed5d61a0b323a17e60b5f3f804a85ec
Jorge Rafael Rosero <rafaelrosero.r.r2408@gmail.com>
2024-03-02 17:45:12 -0500

    config app.js for deploy

--- server/app.js
+++ server/app.js
@@ -4,7 +4,12 @@ import cookieParser from 'cookie-parser';
 import postsRoutes from './routes/posts.routes.js'
 import userRoutes from './routes/users.routes.js'
 import notificationsRoutes from './routes/notifications.routes.js'
+import path from 'path'
 import { createServer } from 'http';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -22,6 +27,14 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Credentials', 'true');
     next();
+});
+
+// Sirve los archivos estáticos desde la carpeta 'dist' de tu aplicación Vite
+app.use(express.static(path.resolve(__dirname, '../client/dist')));
+
+// Asegúrate de que todas las rutas no manejadas sirvan tu archivo index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
 });
 
 
