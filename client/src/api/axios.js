fd5598c767d634810117f5311442c33b0ada3d8c
Jorge Rafael Rosero <rafaelrosero.r.r2408@gmail.com>
2024-03-02 20:57:18 -0500

    updating url for /api request

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : process.env.REACT_APP_API_URL,
+    baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:4000/api/' : 
+    `${process.env.REACT_APP_API_URL}/api`,
     withCredentials: true
 })
 
-export default instance
+export default instance;
--- client/src/context/socketContext.jsx
+++ client/src/context/socketContext.jsx
@@ -17,7 +17,8 @@ export const SocketProvider = ({ children }) => {
 
     useEffect(() => {
         if (user) {
-            const newSocket = io(process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : process.env.REACT_APP_API_URL, {
+            const newSocket = io(process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : 
+            process.env.REACT_APP_API_URL, {
                 query: { userId: user?._id },
             });
             setSocket(newSocket);
