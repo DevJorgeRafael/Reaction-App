efd82cdc4fb16eab1b649910d1a473eef3fb0553
Jorge Rafael Rosero <rafaelrosero.r.r2408@gmail.com>
2024-03-02 19:43:54 -0500

    changing request links for production

--- server/app.js
+++ server/app.js
@@ -22,12 +22,17 @@ app.use(fileUpload({
 app.use(cookieParser())
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    const allowedOrigins = ['http://localhost:5173', 'https://reaction-app.up.railway.app'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Credentials', 'true');
     next();
 });
+
 
 // Sirve los archivos estáticos desde la carpeta 'dist' de tu aplicación Vite
 app.use(express.static(path.resolve(__dirname, '../client/dist')));
